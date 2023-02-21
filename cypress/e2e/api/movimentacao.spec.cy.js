6e086f108826aa3fda12fa6b20e054177deb9eab
A-lineed <aline.edvania.franca@outlook.com>
2023-02-21 11:25:03 -0300

    enter transaction

--- cypress/e2e/api/movimentacao.spec.cy.js
+++ cypress/e2e/api/movimentacao.spec.cy.js
@@ -1,11 +1,39 @@
 /// <reference types="cypress" />
 
 describe('Should test at a API level', () => {
+    let token
     before(() => {
-
+        cy.getToken('aline', '123')
+            .then(tkn => {
+                token = tkn
+            })
+    })
+    beforeEach(() => {
+        cy.resetRest()
     })
 
     it('Should create a transaction', () => {
+        cy.getAccountByName('Conta para movimentacoes')
+            .then(contaId => {
+                cy.request({
+                    method: 'POST',
+                    url: 'https://barrigarest.wcaquino.me/transacoes',
+                    headers: { Authorization: `JWT ${token}` },
+                    body: {
+                        conta_id: contaId,
+                        data_pagamento: "21/02/2023",
+                        data_transacao: "21/02/2023",
+                        descricao: "desc",
+                        envolvido: "inter",
+                        status: true,
+                        tipo: "REC",
+                        valor: "123"
+                    }
+                }).as('response')
 
+            })
+        cy.get('@response').its('status').should('be.equal', 201)
+        cy.get('@response').its('body.id').should('exist')
     })
 })
+
